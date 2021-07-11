6862041fba8e6326b5cbbb45daab5ccb52954fcf
Denis Czaplia <zgibex333@gmail.com>
2021-07-11 17:09:16 +0300

    experience section - done

---
+++ scripts/counter.js
@@ -0,0 +1,42 @@
+const itemsToCount = document.querySelectorAll('.customers');
+const counters = document.querySelector('.point-inner');
+
+document.addEventListener('scroll', startCounter)
+
+function startCounter() {
+    if (window.innerHeight >= counters.getBoundingClientRect().top) {
+        Array.from(itemsToCount).forEach(item => {
+            counter(item)
+        })
+        document.removeEventListener('scroll', startCounter);
+    }
+}
+
+function counter(item) {
+    let stopPoint = Number(item.innerText.slice(0, -1));
+    let counterNum = 0;
+    let duration;
+    if (stopPoint < 20) {
+        duration = 100;
+    } else if (stopPoint < 100) {
+        duration = 50;
+    } else {
+        duration = 10;
+    }
+
+    function increase() {
+        if (counterNum > stopPoint) {
+            setTimeout(() => {
+                item.innerText = `${stopPoint}+`
+            }, duration)
+            return;
+        } else {
+            setTimeout(() => {
+                item.innerText = counterNum;
+                counterNum += 2;
+                increase()
+            }, duration)
+        }
+    }
+    increase()
+}
